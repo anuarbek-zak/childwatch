07ec3ccb790044429b742bbff6398da645576bd0
Anuarbek Zakiryanov <anuarbekzakirianov97@gmail.com>
2017-01-31 17:57:40 +0600

    4

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(bodyParser.json());
 
 
 
-app.post('http://clocklp2.herokuapp.com/endpoint',function (req,res) {
+app.post('/endpoint',function (req,res) {
 	var transport = nodemailer.createTransport({
 		service: 'gmail',
 		auth: {
